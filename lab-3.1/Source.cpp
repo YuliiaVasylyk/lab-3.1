489ca067168ce382eec70c21a26c145ba2f8017c
Yuliia <ylua.vasylyk.04@gmail.com>
2021-10-01 11:11:31 +0300

    hello

--- lab-3.1/Source.cpp
+++ lab-3.1/Source.cpp
@@ -22,9 +22,9 @@ int main()
 
 	// спосіб 1: розгалуження в скороченій формі
 	if (x < 1)
-		B = 0, 65 * x + 8;
+		B = 0.65 * x + 8;
 	if (1 <= x && x < 5)
-		B = atan((x + 6, 1) / 2) + exp(x);
+		B = atan((x + 6.1) / 2) + exp(x);
 	if (x >= 5)
 		B = sqrt(1 + sqrt(x));
 
@@ -36,12 +36,12 @@ int main()
 
 	// спосіб 2: розгалуження в повній формі
 	if (x < 1)
-		B = 0, 65 * x + 8;
+		B = 0.65 * x + 8;
 	else
 		if (x >= 5)
 			B = sqrt(1 + sqrt(x));
 		else
-			B = atan((x + 6, 1) / 2) + exp(x);
+			B = atan((x + 6.1) / 2) + exp(x);
 
 	y = A - B;
 	cout << "2) y = " << y << endl;
